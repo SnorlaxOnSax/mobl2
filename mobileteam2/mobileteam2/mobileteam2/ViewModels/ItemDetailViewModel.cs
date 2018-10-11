7d77ded68d1b4fc2e13d9f28e5721ef2474884c9
Barrett Tenney <tenneyb@grangeinsurance.com>
2018-10-11 09:49:44 -0400

    solution

---
+++ mobileteam2/mobileteam2/mobileteam2/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using mobileteam2.Models;
+
+namespace mobileteam2.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
