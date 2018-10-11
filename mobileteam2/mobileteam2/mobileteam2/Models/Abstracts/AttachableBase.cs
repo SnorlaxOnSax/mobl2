d83c37a793b87f31e592acda20a32f29bfc1656a
Jared Headings <jaredmheadings@gmail.com>
2018-10-11 14:21:30 -0400

    Added some additional framework

---
+++ mobileteam2/mobileteam2/mobileteam2/Models/Abstracts/AttachableBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace mobileteam2.Models.Abstracts
+{
+    public class AttachableBase
+    {
+        public Guid Id { get; set; }
+        public string Path { get; set; }
+    }
+}
