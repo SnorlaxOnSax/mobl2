436e783e2c0de9710f1cda27c21361df3c11b365
IvyMorganLegg <legg.84@osu.edu>
2018-10-12 10:49:43 -0400

    Document Buttons part3

--- mobileteam2/mobileteam2/mobileteam2/Views/PaymentsPage.xaml.cs
+++ mobileteam2/mobileteam2/mobileteam2/Views/PaymentsPage.xaml.cs
@@ -24,13 +24,13 @@ namespace mobileteam2.Views
                 PaymentBtn.Text = "Paid";
                 Deductible.Text = "•Deductible paid on 10/12/18";
                 PaymentBtn.IsEnabled = false;
-                PaymentBtn.BackgroundColor = Color.Gray;
+                PaymentBtn.BackgroundColor = Color.FromHex("#B3B3B3");//LightGrey
             }
 
             else
             {
                 PaymentBtn.IsEnabled = true;
-                PaymentBtn.BackgroundColor = Color.Blue;
+                PaymentBtn.BackgroundColor = Color.FromHex("#2E7D32");//DarkGreen
             }
         }
 
@@ -38,7 +38,7 @@ namespace mobileteam2.Views
         {
             PaymentBtn.Text = "Paid";
             Deductible.Text = "•Deductible paid on 10/12/18";
-            PaymentBtn.BackgroundColor = Color.Gray;
+            PaymentBtn.BackgroundColor = Color.FromHex("#B3B3B3");//LightGrey
             PaymentBtn.IsEnabled = !paymentPaymentViewModel.isDeductiblePaid;
                 if (paymentPaymentViewModel.isDeductiblePaid == false)
                  {
